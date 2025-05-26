3f7b2946c2858be943790f341c44afa031f0b83a
danmend43 <danmend43@gmail.com>
2025-05-26 17:21:20 -0300

    vinteseis

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,9 @@
-import type React from "react"
-import type { Metadata } from "next"
-import "./globals.css"
+import type { Metadata } from 'next'
+import './globals.css'
 
 export const metadata: Metadata = {
-  title: "v0 App",
-  description: "Created with v0",
-  generator: "v0.dev",
+  title: 'Rede',
+  description: 'Rede', 
 }
 
 export default function RootLayout({
